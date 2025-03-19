7325cd3eccbd01bd1106dcb892cab9a10ba4ef9b
eroudini <eroudini.abdillah@yahoo.com>
2025-03-19 12:13:04 +0100

    🤣

--- quizzapp/src/main/java/com/projet/quizzapp/repositories/QuizzRepository.java
+++ quizzapp/src/main/java/com/projet/quizzapp/repositories/QuizzRepository.java
@@ -1,4 +1,4 @@
-package com.projet.quizzapp.repository;
+package com.projet.quizzapp.repositories;
 
 import com.projet.quizzapp.entities.Quizz;
 import org.springframework.data.jpa.repository.JpaRepository;
