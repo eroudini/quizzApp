55569ea189c83941b43a7d2aa2b3752bb9be640d
Luca David <luca.david@etu.univ-tlse2.fr>
2025-03-21 11:59:32 +0100

    Quizz

--- quizzapp-front-end/quizapp/src/App.js
+++ quizzapp-front-end/quizapp/src/App.js
@@ -2,25 +2,20 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Categories from "./Categories";
-import CategoryDetail from "./CategoryDetail";
-import Quiz from "./quizz";
+import Categories from "./pages/Categories";
+import Quiz from "./pages/Quiz";
+import Quizzes from "./pages/Quizzes";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-
         <Route path="/login" element={<Login />} />
-
         <Route path="/register" element={<Register />} />
-
         <Route path="/categories" element={<Categories />} />
-
-        <Route path="/categories/:id" element={<CategoryDetail />} />
-
-        <Route path="/quizz/:id" element={<Quiz />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/quizzes" element={<Quizzes />} />
       </Routes>
     </Router>
   );
